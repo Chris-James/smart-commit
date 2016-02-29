3b1b7aec97cfb25f46bc10dd271fe098da467dde
Chris James <chris@chris-james.me>
2016-02-28 20:31:32 -0500

    TEST(lengthError): Add tests for 1, 2, & 45 char overflows

--- index.test.js
+++ index.test.js
@@ -11,3 +11,9 @@ test('buildBody()', function(t) {
   t.is(index.buildBody(undefined), '', 'handles undefined.');
   t.not(index.buildBody('test').slice(-1), '\n', 'does not concatenate trailing newline.');
 });
+
+test('lengthError()', function(t) {
+  t.is(index.lengthError(1), 'Message was 1 character too long.', 'returns singular.');
+  t.is(index.lengthError(2), 'Message was 2 characters too long.', 'returns plural.');
+  t.is(index.lengthError(45), 'Message was 45 characters too long.', 'returns large plural.');
+});
